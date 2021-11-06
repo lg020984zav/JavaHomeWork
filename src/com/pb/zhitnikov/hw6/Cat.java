052fb9d23335e6bc12f21e680bb77b166979a31e
lg020984zav <andrej.zhitnikov@privatbank.ua>
2021-11-06 22:44:08 +0200

    init commit

---
+++ src/com/pb/zhitnikov/hw6/Cat.java
@@ -0,0 +1,5 @@
+package com.pb.zhitnikov.hw6;
+public class Cat extends Animal {
+    private boolean catWellfed=false;
+
+    }
---
+++ src/com/pb/zhitnikov/hw6/VetСlinic.java
@@ -0,0 +1,14 @@
+package com.pb.zhitnikov.hw6;
+
+public class VetСlinic {
+    public static void main(String[] args) {
+       Animal animal = new Animal();
+       animal.setAnimal("Cat");
+       animal.eat();
+       Cat cat=new Cat();
+       cat.setAnimal("Чорна кішка");
+       cat.eat();
+        // Animal[] animals = new Animal[3]{food,location};
+       // animals[0] = new Cat(Кошачий корм, Дерево);
+    }
+}
