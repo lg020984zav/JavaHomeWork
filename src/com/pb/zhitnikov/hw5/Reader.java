acbb0255a642a8a71b35c6803f60c9866aa5a9c2
lg020984zav <andrej.zhitnikov@privatbank.ua>
2021-11-06 20:57:22 +0200

    init commit

--- src/com/pb/zhitnikov/hw5/Reader.java
+++ src/com/pb/zhitnikov/hw5/Reader.java
@@ -1,6 +1,10 @@
 package com.pb.zhitnikov.hw5;
 public class Reader {
-        private String name;
+    private static int bookCount = 0;
+    public static int getBookCount() {
+        return bookCount;
+    }
+    private String name;
         private int tiketName;
         private String fakultetName;
         private int dateBirth;
@@ -19,6 +23,24 @@ public class Reader {
 
     public String getreaderInfo() {
         return "[ПІБ читача " + name + " " + dateBirth + "р. народження, читацький квиток №" + tiketName +
-                " вчиться " + fakultetName + " номер телефону " + phoneNumber + "]";
+                ", вчиться " + fakultetName + ", номер телефону " + phoneNumber + "]";
+    }
+    public String takeBook (int bookCount) {
+return " взяв книги: " + bookCount;
+           }
+    public String takeBook(Book book1, Book book2, Book book3) {
+        return " взяв книги: "+book1.getBookName()+", "+book2.getBookName()+", "+book3.getBookName();
+    }
+    public String takeBook (Book book1, Book book2, Book book3, Reader reader1) {
+        return " взяв книги: "+book1.getInfo()+", "+book2.getInfo()+", "+book3.getInfo();
+    }
+    public String returnBook (int bookCount) {
+        return " повернув книги: " + bookCount;
+    }
+    public String returnBook (Book book1, Book book2, Book book3) {
+        return " повернув книги: "+book1.getBookName()+", "+book2.getBookName()+", "+book3.getBookName();
+    }
+    public String returnBook (Book book1, Book book2, Book book3, Reader reader1) {
+        return " повернув книги: "+book1.getInfo()+", "+book2.getInfo()+", "+book3.getInfo();
     }
 }
